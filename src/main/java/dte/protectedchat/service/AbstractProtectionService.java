24db01e3c9b92afcc074068356c5a5a963adbe25
David <darknight4123@gmail.com>
2021-07-04 22:13:54 +0300

    Added more default implementations for AbstractProtectionService

--- src/main/java/dte/protectedchat/service/AbstractProtectionService.java
+++ src/main/java/dte/protectedchat/service/AbstractProtectionService.java
@@ -7,6 +7,12 @@ import org.bukkit.entity.Player;
 public abstract class AbstractProtectionService implements ProtectionService 
 {
 	@Override
+	public boolean isProtected(Player player) 
+	{
+		return getProtectedPlayers().contains(player);
+	}
+	
+	@Override
 	public Iterator<Player> iterator() 
 	{
 		return getProtectedPlayers().iterator();
