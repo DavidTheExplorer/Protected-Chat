bae557e254a2cea0b9792c326329d0669bad1059
David <darknight4123@gmail.com>
2021-07-05 17:41:26 +0300

    Added the factory method fromName(String) to ChatHologramProvider

--- src/main/java/dte/protectedchat/holograms/providers/ChatHologramProvider.java
+++ src/main/java/dte/protectedchat/holograms/providers/ChatHologramProvider.java
@@ -1,5 +1,7 @@
 package dte.protectedchat.holograms.providers;
 
+import java.util.Arrays;
+
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -10,4 +12,14 @@ public interface ChatHologramProvider
 	String getName();
 	boolean isAvailable();
 	ChatHologram createHologram(Player owner, Location spawnLocation);
+
+	public static final ChatHologramProvider[] ALL = {new HolographicDisplaysProvider()};
+	
+	public static ChatHologramProvider fromName(String providerName) 
+	{
+		return Arrays.stream(ALL)
+				.filter(provider -> provider.getName().equalsIgnoreCase(providerName))
+				.findFirst()
+				.orElse(null);
+	}
 }
