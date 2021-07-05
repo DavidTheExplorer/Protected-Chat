93a6d778f4feefe03d6c7f02e8285e5299654c42
David <darknight4123@gmail.com>
2021-07-05 15:28:24 +0300

    Added an imple of getPlayersProtecedBy() in AbstractProtectionService

--- src/main/java/dte/protectedchat/service/AbstractProtectionService.java
+++ src/main/java/dte/protectedchat/service/AbstractProtectionService.java
@@ -1,8 +1,13 @@
 package dte.protectedchat.service;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.Collection;
 import java.util.Iterator;
 
 import org.bukkit.entity.Player;
+
+import dte.protectedchat.protectors.ChatProtector;
 
 public abstract class AbstractProtectionService implements ProtectionService 
 {
@@ -10,6 +15,14 @@ public abstract class AbstractProtectionService implements ProtectionService
 	public boolean isProtected(Player player) 
 	{
 		return getProtectedPlayers().contains(player);
+	}
+	
+	@Override
+	public Collection<Player> getPlayersProtectedBy(ChatProtector protector) 
+	{
+		return getProtectedPlayers().stream()
+				.filter(protectedPlayer -> getProtectorOf(protectedPlayer) == protector)
+				.collect(toList());
 	}
 	
 	@Override
